1c36d778cc98c9ea81a9b4776c2ab97699344811
busitora <ts12272530@gmail.com>
2019-03-12 12:36:29 +0900

    beforeアクション、リファクタリング

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [ :show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -18,22 +19,26 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
+    @article.update(article_params)
+    redirect_to @article 
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to articles_path 
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title,:body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
